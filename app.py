b57f075dd36898b242e15762ceb2ae03e233493c
emresvd <emresevindi99@gmail.com>
2026-09-21 14:00:43 +0300

    modified:   app.py	commit.commiter

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.sidebar.header("Analysis Settings")
 scale_factor = st.sidebar.slider("Scale Factor", min_value=0.1, max_value=1.0, value=1.0, step=0.1, help="Reduce image resolution to analyze at a different scale.")
 bits_per_channel = st.sidebar.slider("Color Depth (Bits)", min_value=1, max_value=8, value=8, step=1, help="Reduce color depth to group similar colors together.")
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png", "webp", "bmp", "gif", "tiff"])
 
 if uploaded_file is not None:
     # Read the image
@@ -20,7 +20,7 @@ if uploaded_file is not None:
     
     # Display the input image
     st.subheader("Input Image")
-    st.image(img, caption="Original Image", use_column_width=True)
+    st.image(img, caption="Original Image", use_container_width=True)
     
     st.write("Analyzing...")
     
